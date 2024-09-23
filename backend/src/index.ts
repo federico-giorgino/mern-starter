2dc54b3e1a80a55e217d3681ab780bd1291961cb
Federico Giorgino <fed.giorgino@outlook.com>
2024-09-23 16:26:04 +0200

    feat: Server entrypoint setup with express middlewares

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+import cookieParser from "cookie-parser";
+import path from "path";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+
+app.listen(7000, () => {
+  console.log("server running on localhost:7000");
+});

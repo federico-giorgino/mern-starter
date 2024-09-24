e7b5bede6e06fd2ed3a7599092722417022f9285
Federico Giorgino <fed.giorgino@outlook.com>
2024-09-24 20:18:08 +0200

    feat: auth register test

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -15,3 +15,26 @@ test("should allow the user to sign in", async ({ page }) => {
 
   await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });
+
+test("should allow the user to register", async ({ page }) => {
+  const testEmail = `test_register_${
+    Math.floor(Math.random() * 90000) + 10000
+  }@test.com`;
+
+  await page.goto(UI_URL);
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByRole("link", { name: "Register" }).click();
+
+  await expect(
+    page.getByRole("heading", { name: "Create an account" })
+  ).toBeVisible();
+
+  await page.locator("[name=firstName]").fill("firstName");
+  await page.locator("[name=lastName]").fill("lastName");
+  await page.locator("[name=email]").fill(testEmail);
+  await page.locator("[name=password]").fill("password123");
+
+  await page.getByRole("button", { name: "Register" }).click();
+
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
